04754d0a86cf38594935b6d9e6bf71c95911e242
Garrett Wei <70682725+gw859060@users.noreply.github.com>
2020-11-07 14:03:33 -0500

    bunch of style changes

--- main.js
+++ main.js
@@ -50,8 +50,58 @@ document.addEventListener('DOMContentLoaded', (function () {
         }
     }
 
+    function createCourses() {
+        let request = new XMLHttpRequest();
+        let requestURL = 'https://raw.githubusercontent.com/gw859060/four-year-plan/main/courses.json';
+
+        request.open('GET', requestURL);
+        request.responseType = 'json';
+        request.send();
+        request.addEventListener('load', function () {
+            createCourseObjects();
+        });
+
+        function createCourseObjects() {
+            let crsArray = [];
+            let crs = request.response.years[0].semesters[0].courses[0];
+            let crs2 = request.response.years[0].semesters[0].courses[1];
+
+            let course1 = new Course(crs.subject, crs.number, crs.name, crs.requirement, crs.attribute, crs.credits, 1, 1);
+            let course2 = new Course(crs.subject, crs.number, crs.name, crs.requirement, crs.attribute, crs.credits, 1, 1);
+
+            crsArray.push('course1', 'course2');
+
+            console.log(course1.shorthand());
+
+            // let found1 = crsArray.find(course => {
+            //     course.name.subject === 'CSC';
+            // });
+
+            // crsArray.forEach((obj, i) => {
+            //     console.log(obj);
+            // });
+        }
+
+        function Course(subject, number, fullname, requirement, attribute, credits, year, semester) {
+            this.name = {
+                'subject': subject,
+                'number': number,
+                'fullname': fullname
+            };
+            this.shorthand = function () {
+                return this.name.subject + ' ' + this.name.number;
+            };
+            this.requirement = requirement;
+            this.attribute = attribute;
+            this.credits = credits;
+            this.year = year;
+            this.semester = semester;
+        }
+    }
+
+    // @TODO: make this await createCourses
+    // @TODO: allow user input via localStorage
     function initRequirements() {
-        // @TODO: allow user input via localStorage
         let request = new XMLHttpRequest();
         let requestURL = 'https://raw.githubusercontent.com/gw859060/four-year-plan/main/requirements.json';
 
@@ -94,19 +144,19 @@ document.addEventListener('DOMContentLoaded', (function () {
             let majorElectives = major.electives;
 
             majorCore.forEach((attr, i) => {
-                let parentNode = get('.major .core');
+                let parentNode = get('.section-major .core');
 
                 buildAttrRow(attr, parentNode, i);
             });
 
             majorMath.forEach((attr, i) => {
-                let parentNode = get('.major .mathematics');
+                let parentNode = get('.section-major .mathematics');
 
                 buildAttrRow(attr, parentNode, i);
             });
 
             majorElectives.forEach((attr, i) => {
-                let parentNode = get('.major .electives');
+                let parentNode = get('.section-major .electives');
 
                 buildAttrRow(attr, parentNode, i);
             });
@@ -114,9 +164,9 @@ document.addEventListener('DOMContentLoaded', (function () {
             /* ***** MINOR ***** */
             let minor = request.response.minor[0];
             let minorCore = minor.core;
-            let minorCoreParent = get('.minor .core');
+            let minorCoreParent = get('.section-minor .core');
             let minorElectives = minor.electives;
-            let minorElectiveParent = get('.minor .electives');
+            let minorElectiveParent = get('.section-minor .electives');
 
             // only one attr in each section so no need for forEach
             buildAttrRow(minorCore, minorCoreParent, 0);
@@ -160,8 +210,27 @@ document.addEventListener('DOMContentLoaded', (function () {
         }
     }
 
+    // @TODO: make this await createCourses
+    // @TODO: allow user input via localStorage
+    // @TODO: use WCUPA api
+    //        <https://catalog.wcupa.edu/js/courseleaf.js>
+    //        <https://catalog.wcupa.edu/ribbit/index.cgi?page=getcourse.rjs&code=CSC%20142>
+    //        <https://stackoverflow.com/questions/5031501/how-to-rate-limit-ajax-requests>
+    // @TODO: toggle switch for compact/expanded views
+    // @TODO: show a vertical timeline w/ dotted line, layout flipping sides every other year
+    //        <https://css-tricks.com/building-a-conference-schedule-with-css-grid/>
+    //        <https://codyhouse.co/demo/schedule-template/index.html>
+    //                        FALL 2020
+    //         SEMESTER 1 YEAR 1     SEMESTER 2 YEAR 1
+    //        [   course list   ]   [   course list   ]
+    //        [ hourly schedule ]   [ hourly schedule ]
+    //
+    //                       SPRING 2020
+    //         SEMESTER 3 YEAR 2     SEMESTER 4 YEAR 2
+    //        [   course list   ]   [   course list   ]
+    //        [ hourly schedule ]   [ hourly schedule ]
+
     function initSchedule() {
-        // @TODO: allow user input via localStorage
         let request = new XMLHttpRequest();
         let requestURL = 'https://raw.githubusercontent.com/gw859060/four-year-plan/main/courses.json';
 
@@ -183,7 +252,7 @@ document.addEventListener('DOMContentLoaded', (function () {
                 let yearNum = year.year;
                 let yearSection = get('.year', yearTemplate);
 
-                get('main').appendChild(yearTemplate);
+                get('.course-schedule > *').appendChild(yearTemplate);
 
                 /* ***** SEMESTERS ***** */
 
@@ -232,84 +301,77 @@ document.addEventListener('DOMContentLoaded', (function () {
                     if (semesterNum === 1) semesterYear++;
                 });
             });
+        }
 
-            function linkCourseName(parentNode, subject, number, name) {
-                let link = document.createElement('a');
+        function linkCourseName(parentNode, subject, number, name) {
+            let link = document.createElement('a');
 
-                link.classList.add('course-link');
-                link.setAttribute('target', '_blank');
-                link.setAttribute('ref', 'noopener');
-                link.setAttribute('title', `Open in course catalog on wcupa.edu`);
-                link.href = `https://catalog.wcupa.edu/search/?P=${subject}+${number}`;
-                link.textContent = name;
-                parentNode.appendChild(link);
+            link.classList.add('course-link');
+            link.setAttribute('target', '_blank');
+            link.setAttribute('ref', 'noopener');
+            link.setAttribute('title', `Open in course catalog on wcupa.edu`);
+            link.href = `https://catalog.wcupa.edu/search/?P=${subject}+${number}`;
+            link.textContent = name;
+            parentNode.appendChild(link);
+        }
+
+        function handleReq(parentNode, abbrev) {
+            // if course contains multiple attributes
+            if (typeof abbrev === 'object') {
+                abbrev.forEach(attr => buildPill(parentNode, attr));
+            } else {
+                buildPill(parentNode, abbrev);
             }
 
-            function handleReq(parentNode, abbrev) {
-                // if course contains multiple attributes
-                if (typeof abbrev === 'object') {
-                    abbrev.forEach(attr => buildPill(parentNode, attr));
-                } else {
-                    buildPill(parentNode, abbrev);
+            function buildPill(parentNode, attr) {
+                let pill = document.createElement('button');
+
+                pill.setAttribute('type', 'button');
+                pill.classList.add('pill', attr);
+                pill.textContent = expandAbbrev(attr);
+                pill.addEventListener('click', highlightTiles(), false);
+
+                parentNode.appendChild(pill);
+            }
+        }
+
+        function highlightTiles() {
+            return function () {
+                // if target pill is not already selected
+                if (this.classList.contains('selected') === false) {
+                    let semesters = getAll('.semesters');
+                    let selectedPills = getAll('.pill.' + this.classList[1]);
+
+                    // clear existing selections before highlighting new ones
+                    // (ie. only one selection at a time)
+                    clearSelected();
+
+                    semesters.forEach(semester => semester.classList.add('filtered'));
+
+                    selectedPills.forEach(pill => {
+                        pill.classList.add('selected');
+                        pill.closest('.course').classList.add('selected');
+                    });
                 }
-
-                function buildPill(parentNode, attr) {
-                    let pill = document.createElement('button');
-
-                    pill.setAttribute('type', 'button');
-                    pill.classList.add('pill', attr);
-                    pill.textContent = expandAbbrev(attr);
-                    pill.addEventListener('click', highlightTiles(), false);
-
-                    parentNode.appendChild(pill);
+                // otherwise you're clicking a selected pill
+                else {
+                    clearSelected();
                 }
             }
 
-            function highlightTiles() {
-                return function () {
-                    // if target pill is not already selected
-                    if (this.classList.contains('selected') === false) {
-                        let semesters = getAll('.semesters');
-                        let selectedPills = getAll('.pill.' + this.classList[1]);
+            function clearSelected() {
+                let selectedPills = getAll('.pill.selected');
+                let selectedCourses = getAll('.course.selected');
+                let filteredSems = getAll('.semesters.filtered');
 
-                        // clear existing selections before highlighting new ones
-                        // (ie. only one selection at a time)
-                        clearSelected();
-
-                        semesters.forEach(semester => semester.classList.add('filtered'));
-
-                        selectedPills.forEach(pill => {
-                            pill.classList.add('selected');
-                            pill.closest('.course').classList.add('selected');
-                        });
-                    }
-                    // otherwise you're clicking a selected pill
-                    else {
-                        clearSelected();
-                    }
-                }
-
-                function clearSelected() {
-                    let selectedPills = getAll('.pill.selected');
-                    let selectedCourses = getAll('.course.selected');
-                    let filteredSems = getAll('.semesters.filtered');
-
-                    selectedPills.forEach(pill => pill.classList.remove('selected'));
-                    selectedCourses.forEach(course => course.classList.remove('selected'));
-                    filteredSems.forEach(semester => semester.classList.remove('filtered'));
-                }
+                selectedPills.forEach(pill => pill.classList.remove('selected'));
+                selectedCourses.forEach(course => course.classList.remove('selected'));
+                filteredSems.forEach(semester => semester.classList.remove('filtered'));
             }
         }
     }
 
-    function cleanUp() {
-        get('noscript').remove();
-        getAll('template').forEach(template => template.remove());
-    }
-
+    // createCourses();
     initRequirements();
     initSchedule();
-    // needs to wait until two inits are done
-    // <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function>
-    // cleanUp();
 })(), false);
