96958636c75a7ac8948dccd93734c5c7a61a6070
Garrett Wei <70682725+gw859060@users.noreply.github.com>
2020-11-25 21:33:19 -0500

    add tooltips to requirements section

--- scripts/main.js
+++ scripts/main.js
@@ -95,18 +95,24 @@
             this.credits = credits;
             this.year = year;
             this.semester = semester;
-            // this.tooltip = function () {
-            //     let tooltip = document.createElement('div');
-            //
-            //     tooltip.classList.add('tooltip');
-            //     tooltip.textContent = 'test';
-            //     return tooltip;
-            // }
+            this.tooltip = function () {
+                let tooltip = document.createElement('div');
+                let details = document.createElement('div');
+
+                tooltip.classList.add('tooltip', 'tile', 'dark');
+                tooltip.textContent = `${this.name.shorthand}: ${title}`;
+
+                details.classList.add('tooltip-details');
+                details.textContent = `Year ${year}, ${expandSemester(semester)} Semester`;
+                tooltip.appendChild(details);
+
+                return tooltip;
+            }
         }
     }
 
     function buildRequirements(json) {
-        /* ***** GEN EDS ***** */
+        /* ***** gen eds ***** */
         let geneds = json.gened[0];
         let genedTypes = ['academic', 'distributive', 'additional'];
 
@@ -118,7 +124,7 @@
             });
         }
 
-        /* ***** MAJOR ***** */
+        /* ***** major ***** */
         let major = json.major[0];
         let majorTypes = ['core', 'mathematics', 'electives'];
 
@@ -130,7 +136,7 @@
             });
         }
 
-        /* ***** MINOR ***** */
+        /* ***** minor ***** */
         let minor = json.minor[0];
 
         // only one attr in each section so no need for forEach
@@ -173,7 +179,7 @@
     }
 
     function fillRequirements(courses) {
-        // fill in courses
+        /* ***** fill in courses ***** */
         for (let course of courses) {
             let requirement = course.reqs.requirement;
             let attribute = course.reqs.attribute;
@@ -182,13 +188,10 @@
             // eg. CSC 301 is major and gen ed
             if (typeof requirement === 'object') {
                 requirement.forEach((req, i) => {
-                    let sectionNode = '';
-
-                    if (req !== 'gened') sectionNode = '.section-' + req;
-
+                    let sectionNode = (req !== 'gened') ? '.section-' + req : '';
                     let reqNode = get(`.requirement-${req} ${sectionNode} .attribute.${attribute[i]} .attribute-course`);
 
-                    doThing(course, reqNode);
+                    addRow(course, reqNode);
                 });
             }
             // if course meets multiple attributes
@@ -197,20 +200,17 @@
                 attribute.forEach(attr => {
                     let reqNode = get(`.requirement-${requirement} .attribute.${attr} .attribute-course`);
 
-                    doThing(course, reqNode);
+                    addRow(course, reqNode);
                 });
             }
             // otherwise course has a single requirement/attribute pair
             else {
-                let sectionNode = '';
-
-                if (requirement !== 'gened') sectionNode = '.section-' + requirement;
-
+                let sectionNode = (requirement !== 'gened') ? '.section-' + requirement : '';
                 let reqNode = get(`.requirement-${requirement} ${sectionNode} .attribute.${attribute} .attribute-course`);
 
                 // handle math separately to allow them to keep the same pill text
                 if (requirement === 'major' && attribute === 'math') {
-                    let math;
+                    let math = '';
 
                     if (course.name.title.includes('Statistics')) math = 'statistics';
                     if (course.name.title.includes('Calculus')) math = 'calculus';
@@ -218,11 +218,11 @@
                     reqNode = get(`.requirement-major .attribute.${math} .attribute-course`);
                 }
 
-                doThing(course, reqNode);
+                addRow(course, reqNode);
             }
         }
 
-        // fill in summary section
+        /* ***** fill in summary section ***** */
         let reqTypes = ['gened', 'major', 'minor'];
         let courseTotal = 0;
         let creditTotal = 0;
@@ -250,59 +250,56 @@
         let courseNode = get(`.requirement.courses .total .attribute-course`);
         let creditNode = get(`.requirement.credits .total .attribute-course`);
 
-        courseNode.textContent = courses.length; // doesn't count overlap between types
+        courseNode.textContent = courses.length; // don't count overlap between types
         courseNode.title = 'May be less than the total of the numbers shown above, due to overlap between requirements.'
         creditNode.textContent = creditTotal;
 
-        // add checkmark to filled tiles
-        checkRequirements();
+        /* ***** add checkmark to filled tiles ***** */
+        let tiles = getAll(':not(.section-summary) > .tile.requirement');
 
-        function doThing(course, node) {
+        for (let tile of tiles) {
+            let attributes = getAll('.attribute-course', tile);
+            let abort = false;
+
+            for (let attribute of attributes) {
+                if (attribute.textContent === '—') {
+                    abort = true;
+                    break;
+                }
+            }
+
+            // skip this tile if it has an unfilled attribute
+            if (abort === true) continue;
+
+            // add checkmark if all requirements in this tile are filled
+            let check = document.createElement('span');
+
+            check.setAttribute('title', 'This section\'s requirements have been filled.');
+            check.classList.add('icon', 'checkmark');
+            get('h4', tile).appendChild(check);
+        }
+
+        function addRow(course, node) {
             // if attribute is already filled, move to the next one
             while (node.textContent !== '—') {
                 node = get('.attribute-course', node.parentNode.nextElementSibling);
             }
 
             node.textContent = course.name.shorthand;
-            node.setAttribute('title', course.name.shorthand + ': ' + course.name.title);
+            node.appendChild(course.tooltip());
 
             // on click, jump to course schedule and highlight the course
             node.classList.add('linked');
             node.addEventListener('click', function () {
                 let clickedCourse = get('#' + course.name.id);
+                let motion = (window.matchMedia('(prefers-reduced-motion)').matches) ? 'auto' : 'smooth';
 
-                clickedCourse.closest('.semester').scrollIntoView({ behavior: 'smooth' });
+                clickedCourse.closest('.semester').scrollIntoView({ behavior: motion });
                 clickedCourse.classList.add('highlighted');
                 window.setTimeout(function () {
                     clickedCourse.classList.remove('highlighted');
                 }, 3000);
             }, false);
-        }
-
-        function checkRequirements() {
-            let tiles = getAll(':not(.section-summary) > .tile.requirement');
-
-            for (let tile of tiles) {
-                let attributes = getAll('.attribute-course', tile);
-                let abort = false;
-
-                for (let attribute of attributes) {
-                    if (attribute.textContent === '—') {
-                        abort = true;
-                        break;
-                    }
-                }
-
-                // skip this tile if it has an unfilled attribute
-                if (abort === true) continue;
-
-                // add checkmark if all requirements in this tile are filled
-                let check = document.createElement('span');
-
-                check.setAttribute('title', 'This section\'s requirements have been filled');
-                check.classList.add('icon', 'checkmark');
-                get('h4', tile).appendChild(check);
-            }
         }
     }
 
@@ -360,23 +357,8 @@
                 // create empty semester section
                 let semTemplate = get('.template-semester').content.cloneNode(true);
                 let semHeader = get('.semester-num', semTemplate);
-                let season = '';
+                let season = expandSemester(semester);
                 let semCreditTotal = 0;
-
-                switch (semester) {
-                    case 1:
-                        season = 'Fall';
-                        break;
-                    case 2:
-                        season = 'Spring';
-                        break;
-                    case 3:
-                        season = 'Summer';
-                        break;
-                    case 4:
-                        season = 'Winter';
-                        break;
-                }
 
                 semHeader.innerHTML = `Year ${year} <span class="subdued">${season} ${semesterYear}</span>`;
                 if (semester === 1) semesterYear++;
@@ -398,16 +380,15 @@
                     courseNode.id = course.name.id;
                     shortNode.textContent = course.name.shorthand;
                     shortNodeAlt.textContent = course.name.shorthand;
-                    linkCourse(titleNode, course);
+                    linkTitle(titleNode, course);
                     handlePill(reqContainer, course.reqs.requirement);
                     handlePill(reqContainer, course.reqs.attribute);
                     creditsNode.textContent = course.credits + ' cr.';
                     semCreditTotal += course.credits;
-                    course.node = function () {
+                    get('.semester', semTemplate).appendChild(courseTemplate);
+                    course.tile = function () {
                         return courseNode;
                     };
-                    get('.semester', semTemplate).appendChild(courseTemplate);
-                    // @TODO: on hover, add tooltip element
 
                     // fade out overflowing pills
                     let reqObserver = new ResizeObserver(function () {
@@ -432,7 +413,7 @@
             }
         }
 
-        function linkCourse(container, course) {
+        function linkTitle(container, course) {
             let link = document.createElement('a');
 
             link.classList.add('course-link');
@@ -444,12 +425,12 @@
             container.appendChild(link);
         }
 
-        function handlePill(container, req) {
+        function handlePill(container, requirement) {
             // if course meets multiple requirements/attributes
-            if (typeof req === 'object') {
-                req.forEach(attr => buildPill(container, attr));
+            if (typeof requirement === 'object') {
+                requirement.forEach(req => buildPill(container, req));
             } else {
-                buildPill(container, req);
+                buildPill(container, requirement);
             }
 
             function buildPill(container, req) {
@@ -582,6 +563,19 @@
 
         function capitalizeFirstLetter(string) {
             return string[0].toUpperCase() + string.slice(1);
+        }
+    }
+
+    function expandSemester(semester) {
+        switch (semester) {
+            case 1:
+                return 'Fall';
+            case 2:
+                return 'Spring';
+            case 3:
+                return 'Summer';
+            case 4:
+                return 'Winter';
         }
     }
 
