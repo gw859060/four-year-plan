9214d552ffdc6e47b9acb366774c2c5a1de4938e
gw859060 <70682725+gw859060@users.noreply.github.com>
2021-03-18 20:28:43 -0400

    move tile code into Course.tile() method; add undated courses section; add message when js is disabled

--- scripts/main.js
+++ scripts/main.js
@@ -42,9 +42,11 @@
             let requirements = data[0];
             let courses = createCourses(data[1]);
 
+            buildNavigation();
             buildRequirements(requirements);
             fillRequirements(courses);
             buildSchedule(courses);
+            buildUndated(courses);
             buildSummary(courses);
         })
         .catch(console.log);
@@ -142,6 +144,24 @@
             tooltip.appendChild(details);
 
             return tooltip;
+        };
+        this.tile = function () {
+            let template = get('.template-course').content.cloneNode(true),
+                courseNode = get('.course', template),
+                shortNode = get('.course-shorthand', template),
+                shortNodeAlt = get('.course-shorthand-alt', template),
+                titleNode = get('.course-name', template),
+                reqContainer = get('.req-container', template),
+                creditsNode = get('.course-credits', template);
+
+            courseNode.id = this.name.id;
+            shortNode.textContent = this.name.shorthand;
+            shortNodeAlt.textContent = this.name.shorthand + ' ';
+            linkTitle(titleNode, this);
+            addPills(reqContainer, this);
+            creditsNode.textContent = this.credits + ' cr.';
+
+            return template;
         };
     }
 
@@ -338,7 +358,16 @@
                 // @TODO: <https://css-tricks.com/smooth-scrolling-accessibility/>
                 //        problem: setting focus as instructed makes the browser jump to target immediately,
                 //        skipping the smooth scroll behavior; alternatively, just remove smooth scroll
-                get('.semester-num', clickedCourse.parentNode).scrollIntoView({ behavior: motion });
+                let parentHeader = get('.semester-num', clickedCourse.parentNode);
+
+                // all courses in "Course Schedule" section
+                if (parentHeader) {
+                    parentHeader.scrollIntoView({ behavior: motion });
+                }
+                // all courses in "Undated Courses" section (no semester header)
+                else {
+                    clickedCourse.parentNode.scrollIntoView({ behavior: motion });
+                }
 
                 window.setTimeout(function () {
                     get('.req-container', clickedCourse).classList.remove('no-fade');
@@ -383,7 +412,6 @@
     //        CORS workaround:
     //            <https://app.cors.bridged.cc/>
     //            <https://medium.com/bridgedxyz/cors-anywhere-for-everyone-free-reliable-cors-proxy-service-73507192714e>
-    // @TODO: show transfer/other courses (ie. courses without a semester) in separate section?
 
     function buildSchedule(courses) {
         // starting year for "Fall 2020" headers; I know it's a bad name
@@ -415,7 +443,6 @@
 
             // fill year with semesters
             for (let semester of semList) {
-                let courseIndex = 0;
                 let courseList = courses.filter(c => c.year === year).filter(c => c.semester === semester);
 
                 // create empty semester section
@@ -428,22 +455,7 @@
 
                 // fill semester with courses
                 for (let course of courseList) {
-                    let courseTemplate = get('.template-course').content.cloneNode(true),
-                        courseNode = get('.course', courseTemplate),
-                        shortNode = get('.course-shorthand', courseTemplate),
-                        shortNodeAlt = get('.course-shorthand-alt', courseTemplate),
-                        titleNode = get('.course-name', courseTemplate),
-                        reqContainer = get('.req-container', courseTemplate),
-                        creditsNode = get('.course-credits', courseTemplate);
-
-                    courseNode.id = course.name.id;
-                    shortNode.textContent = course.name.shorthand;
-                    shortNodeAlt.textContent = course.name.shorthand + ' ';
-                    linkTitle(titleNode, course);
-                    addPills(reqContainer, course);
-                    creditsNode.textContent = course.credits + ' cr.';
-                    semNode.appendChild(courseTemplate);
-                    courseIndex++;
+                    semNode.appendChild(course.tile());
                 }
 
                 // check if at least one course in the semester has course times before building week view
@@ -478,110 +490,6 @@
 
                 container.addEventListener('keydown', makeAccessible);
             });
-        }
-
-        function linkTitle(container, course) {
-            let link = document.createElement('a');
-
-            link.classList.add('course-link');
-            link.setAttribute('target', '_blank');
-            link.setAttribute('rel', 'noopener');
-            link.setAttribute('title', `See description in WCUPA Course Catalog`);
-            link.href = `https://catalog.wcupa.edu/search/?P=${course.name.subject}+${course.name.number}`;
-            link.textContent = course.name.title;
-            container.appendChild(link);
-        }
-
-        function addPills(container, course) {
-            let requirement = course.reqs.requirement;
-            let attribute = course.reqs.attribute;
-
-            // skip courses that don't meet any attributes
-            // @TODO: replace with some text like "no requirements" to maintain the same height
-            if (requirement === 'other') return;
-
-            // if course meets multiple requirements
-            // eg. CSC 301 is major and gen ed
-            if (typeof requirement === 'object') {
-                requirement.forEach((req, i) => {
-                    buildPill(container, req, attribute[i]);
-                });
-            }
-            // if course meets multiple attributes
-            // eg. GEO 204 is i and j
-            else if (typeof attribute === 'object') {
-                attribute.forEach((attr, i) => {
-                    if (i === 0) {
-                        buildPill(container, requirement, attr);
-                    }
-                    // only add remaining attr, not a duplicate requirement + attr
-                    // eg. GEO 204 should show [gened] [i] [j] instead of [gened] [i] [gened] [j]
-                    else {
-                        buildPill(container, requirement, attr, true);
-                    }
-                });
-            } else {
-                buildPill(container, requirement, attribute);
-            }
-
-            // fade out pills if they overflow the container
-            let reqObserver = new ResizeObserver(entry => {
-                let el = entry[0];
-
-                if (el.target.scrollWidth > el.target.offsetWidth) {
-                    el.target.classList.add('fade-overflow');
-                } else if (el.target.classList.contains('fade-overflow')) {
-                    el.target.classList.remove('fade-overflow');
-                }
-            });
-
-            reqObserver.observe(container);
-
-            function buildPill(container, req, attr, duplicate = false) {
-                for (let type of [req, attr]) {
-                    // skip duplicate requirements
-                    if (type === req && duplicate === true) {
-                        continue;
-                    }
-
-                    let pill = document.createElement('button');
-                    // separate attributes with the same name that appear in multiple reqs
-                    // (eg. core in both major/minor) by making attr classes more specific
-                    let typeClass = (type === attr) ? `${req}-${type}` : type;
-
-                    pill.classList.add('pill', typeClass);
-                    pill.textContent = expandAbbrev(type);
-                    pill.setAttribute('type', 'button');
-                    pill.setAttribute('tabindex', '-1');
-                    pill.addEventListener('click', function () {
-                        // highlight courses with same req/attr, fade all others
-                        if (this.classList.contains('selected') === false) {
-                            clearSelected();
-
-                            let years = getAll('.year');
-                            let selectedPills = getAll('.pill.' + this.classList[1]);
-
-                            for (let year of years) year.classList.add('filtered');
-
-                            for (let pill of selectedPills) {
-                                pill.classList.add('selected');
-                                pill.closest('.course').classList.add('selected');
-                            }
-                        }
-                        // if clicked pill is already selected, clear selection
-                        else {
-                            clearSelected();
-                        }
-                    }, false);
-
-                    container.appendChild(pill);
-                }
-
-                function clearSelected() {
-                    getAll('.selected').forEach(el => el.classList.remove('selected'));
-                    getAll('.filtered').forEach(el => el.classList.remove('filtered'));
-                }
-            }
         }
 
         function buildWeek(container, courses) {
@@ -660,10 +568,12 @@
             // @TODO: only for current semester? would require setting semester start/end dates
             let dayHeaders = getAll('.header-day', container);
             let todayNum = new Date().getDay();
+            let todayNode = dayHeaders[todayNum - 1]; // subtract 1 because week starts on monday, not sunday
 
-            // ignore the weekend; subtract 1 because schedule starts on monday, not sunday
-            if (dayHeaders[todayNum - 1]) {
-                dayHeaders[todayNum - 1].classList.add('today');
+            // skip days that don't exist (ie. the weekend)
+            if (todayNode) {
+                todayNode.classList.add('today');
+                todayNode.setAttribute('aria-label', todayNode.getAttribute('aria-label') + ' (today)');
             }
 
             // set css variable for use in .header-hours and .week-grid
@@ -822,6 +732,14 @@
         }
     }
 
+    function buildUndated(courses) {
+        let filteredCourses = courses.filter(course => course.times === null);
+
+        for (let course of filteredCourses) {
+            get('.undated-courses-container').appendChild(course.tile());
+        }
+    }
+
     function buildSummary(courses) {
         // subtotals for requirement types
         let reqTypes = ['gened', 'major', 'minor', 'other'];
@@ -856,6 +774,117 @@
 
         courseNode.textContent = courses.length; // don't count overlap between types
         creditNode.textContent = creditTotal;
+    }
+
+    function linkTitle(container, course) {
+        let link = document.createElement('a');
+
+        link.classList.add('course-link');
+        link.setAttribute('target', '_blank');
+        link.setAttribute('rel', 'noopener');
+        link.setAttribute('title', `See description in WCUPA Course Catalog`);
+        link.href = `https://catalog.wcupa.edu/search/?P=${course.name.subject}+${course.name.number}`;
+        link.textContent = course.name.title;
+        container.appendChild(link);
+    }
+
+    function addPills(container, course) {
+        let requirement = course.reqs.requirement;
+        let attribute = course.reqs.attribute;
+
+        // skip courses that don't meet any attributes
+        // @TODO: replace with some text like "no requirements" to maintain the same height
+        if (requirement === 'other') {
+            container.classList.add('no-reqs');
+            container.textContent = 'None';
+            return;
+        }
+
+        // if course meets multiple requirements
+        // eg. CSC 301 is major and gen ed
+        if (typeof requirement === 'object') {
+            requirement.forEach((req, i) => {
+                buildPill(container, req, attribute[i]);
+            });
+        }
+        // if course meets multiple attributes
+        // eg. GEO 204 is i and j
+        else if (typeof attribute === 'object') {
+            attribute.forEach((attr, i) => {
+                if (i === 0) {
+                    buildPill(container, requirement, attr);
+                }
+                // only add remaining attr, not a duplicate requirement + attr
+                // eg. GEO 204 should show [gened] [i] [j] instead of [gened] [i] [gened] [j]
+                else {
+                    buildPill(container, requirement, attr, true);
+                }
+            });
+        } else {
+            buildPill(container, requirement, attribute);
+        }
+
+        // fade out pills if they overflow the container
+        let reqObserver = new ResizeObserver(entry => {
+            let el = entry[0];
+
+            if (el.target.scrollWidth > el.target.offsetWidth) {
+                el.target.classList.add('fade-overflow');
+            } else if (el.target.classList.contains('fade-overflow')) {
+                el.target.classList.remove('fade-overflow');
+            }
+        });
+
+        reqObserver.observe(container);
+
+        function buildPill(container, req, attr, duplicate = false) {
+            for (let type of [req, attr]) {
+                // skip duplicate requirements
+                if (type === req && duplicate === true) {
+                    continue;
+                }
+
+                let pill = document.createElement('button');
+                // separate attributes with the same name that appear in multiple reqs
+                // (eg. core in both major/minor) by making attr classes more specific
+                let typeClass = (type === attr) ? `${req}-${type}` : type;
+
+                pill.classList.add('pill', typeClass);
+                pill.textContent = expandAbbrev(type);
+                pill.setAttribute('type', 'button');
+                pill.setAttribute('tabindex', '-1');
+                pill.addEventListener('click', function (e) {
+                    // highlight courses with same req/attr, fade all others
+                    if (e.target.classList.contains('selected') === false) {
+                        clearSelectedPills();
+
+                        let years = getAll('.year');
+                        let selectedPills = getAll('.pill.' + this.classList[1]);
+
+                        // for "Course Schedule" section
+                        for (let year of years) year.classList.add('filtered');
+                        // for "Undated Courses" section
+                        get('.undated-courses-container').classList.add('filtered');
+
+                        for (let pill of selectedPills) {
+                            pill.classList.add('selected');
+                            pill.closest('.course').classList.add('selected');
+                        }
+                    }
+                    // else if clicked pill is already selected, clear selection
+                    else {
+                        clearSelectedPills();
+                    }
+                }, false);
+
+                container.appendChild(pill);
+            }
+
+            function clearSelectedPills() {
+                getAll('.selected').forEach(el => el.classList.remove('selected'));
+                getAll('.filtered').forEach(el => el.classList.remove('filtered'));
+            }
+        }
     }
 
     function makeAccessible(event) {
@@ -956,6 +985,8 @@
         }
     }
 
+    // remove class that hides content when js is not enabled
+    get('html').classList.remove('no-js');
+
     fetchData();
-    buildNavigation();
 }());
